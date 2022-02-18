168e197c316038f27fbb809c2891dd747b5dd3d8
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-02-18 16:08:37 -0300

    add error-middleware file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = require('./routes');
+const err = require('../src/layers/middlewares/err');
 
 const app = express();
 app.use(express.json());
+
 app.use(router);
+app.use(err);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`On at: ${PORT}!`));
