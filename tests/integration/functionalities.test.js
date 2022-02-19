6e7186d2d3d15e354662df6ef0a56f0e966760c3
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-02-19 19:12:22 -0300

    add initial test to product-register route

--- tests/integration/functionalities.test.js
+++ tests/integration/functionalities.test.js
@@ -8,12 +8,14 @@ const { getConnection } = require('./connectionMock');
 chai.use(chaiHttp);
 const { expect } = chai;
 
+
 let connectionMock;
 before(async () => {
   connectionMock = await getConnection();
   sinon.stub(mongodb, 'connect').resolves(connectionMock);
 });
 after(() => { mongodb.connect.restore() });
+
 
 describe('Cadastro de novos ingredientes no estoque', () => {
   let response;
@@ -60,6 +62,55 @@ describe('Cadastro de novos ingredientes no estoque', () => {
 
     it('A requisição deve retornar a mensagem: "name" is required', () => {
       expect(response.body.message).to.be.equals('"name" is required');
+    })
+
+  });
+
+});
+
+describe('Cadastro de novos produtos', () => {
+  let response;
+
+  describe('Testa quando são informadas entradas válidas para cadastro de um novo produto', () => {
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/product/new')
+        .send({
+          name: 'Pipoca média',
+          salePrice: 10,
+          ingredients: { milho: 0.5, manteiga: 0.05 },
+        });
+    });
+
+    it('A requisição deve retornar o status 201', () => {
+      expect(response).to.have.status(201);
+    })
+
+    it('A requisição deve retornar um objeto com um id', () => {
+      expect(response.body).to.be.an('object');
+      expect(response.body).to.haveOwnProperty('id');
+    })
+
+  });
+
+  describe('Testa quando não é informado um preço de venda ao tentar registrar de um novo produto', () => {
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/product/new')
+        .send({
+          name: 'Pipoca grande',
+          ingredients: { milho: 0.85, manteiga: 0.075 },
+        });
+    });
+
+    it('A requisição deve retornar o status 400', () => {
+      expect(response).to.have.status(400);
+    })
+
+    it('A requisição deve retornar a mensagem: "salePrice" is required', () => {
+      expect(response.body.message).to.be.equals('"salePrice" is required');
     })
 
   });
